cd31f60d85b39cbaee4ad3ae9ccc926ac5e393a1
Baotong Zhuang <baotong.zhuang@gmail.com>
2013-02-26 16:44:13 +0800

    add yaml-mode in emacs.el

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -784,6 +784,13 @@
 ;; )
 
 ;;;; 10. general programming  setting
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+(add-hook 'yaml-mode-hook
+          '(lambda ()
+             (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
 (require 'protobuf-mode)
 (add-to-list 'auto-mode-alist '("\\.proto\\'" . protobuf-mode))
 
